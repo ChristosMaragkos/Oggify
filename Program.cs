db5854eee57bbbefc459d9322ccc6acd198d7c03
WhiteTowerGames <whitetowersoftware@gmail.com>
2025-06-23 23:25:12 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Oggify;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        var ffmpegPath = @"C:\Program Files (x86)\FFmpeg for Audacity\ffmpeg.exe";
+        var inputPath = @"D:\Sounds\input.wav";
+        var outputPath = @"D:\Sounds\output.ogg";
+
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = ffmpegPath,
+                Arguments = $"-i \"{inputPath}\" -c:a libvorbis -qscale:a 5 \"{outputPath}\"",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+
+        };
+
+    }
+    
+}
